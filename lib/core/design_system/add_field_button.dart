6965153391ee3b0246aa7c817530ddc4f3b2f063
orlando_bressan_k2+1-at-032553173048 <or.bressan@hotmail.com>
2024-12-02 17:49:41 -0300

    minor correction

--- lib/core/design_system/add_field_button.dart
+++ lib/core/design_system/add_field_button.dart
@@ -7,40 +7,37 @@ class AddFieldButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: DottedBorder(
-            color: ProjectColors.gray400,
-            borderType: BorderType.RRect,
-            radius: const Radius.circular(12),
-            dashPattern: const [6, 3],
-            strokeWidth: 1.5,
-            child: Container(
-              color: ProjectColors.gray100,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-              child: const Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.add,
-                    color: ProjectColors.gray800,
-                    size: 20,
-                  ),
-                  SizedBox(width: 8),
-                  Text(
-                    'Add Field',
-                    style: TextStyle(
-                      color: ProjectColors.gray800,
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
+    return Expanded(
+      child: DottedBorder(
+        color: ProjectColors.gray400,
+        borderType: BorderType.RRect,
+        radius: const Radius.circular(12),
+        dashPattern: const [6, 3],
+        strokeWidth: 1.5,
+        child: Container(
+          width: double.infinity,
+          color: ProjectColors.gray100,
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+          child: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.add,
+                color: ProjectColors.gray800,
+                size: 20,
               ),
-            ),
+              SizedBox(width: 8),
+              Text(
+                'Add Field',
+                style: TextStyle(
+                  color: ProjectColors.gray800,
+                  fontSize: 12,
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
